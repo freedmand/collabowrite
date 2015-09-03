407c840982824193af41d6daeff37f9d00828137
Dylan Freedman <freedmand@gmail.com>
2015-09-03 18:51:18 +0000

    Basic nav

--- collabowrite/collabowrite.js
+++ collabowrite/collabowrite.js
@@ -1,17 +1,40 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+APPNAME = "CollaboWrite";
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+Router.route('/', {
+    template: 'home',
+    name: 'home'
+});
+
+Router.route('/register');
+Router.route('/login');
+
+if (Meteor.isClient) {
+  document.title = APPNAME;
+  
+  Template.appname.helpers({
+    'name': APPNAME
+  });
+  
+  Template.footer.helpers({
+    'year': function() {
+      return '' + (new Date().getYear() + 1900);
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  
+  Template.register.events({
+    'submit form': function(){
+      event.preventDefault();
+      var email = $('[name=email]').val();
+      var password = $('[name=password]').val();
+      Accounts.createUser({
+        email: email,
+        password: password
+      });
+      Router.go('login');
     }
   });
 }
